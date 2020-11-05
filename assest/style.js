ad2d88a9a9de1e7272f3ff87cb099a64b178a21c
Vivek Chauhan <48582432+Vivekagent47@users.noreply.github.com>
2020-11-05 20:20:14 +0530

    Updates

---
+++ assest/style.js
@@ -0,0 +1,14 @@
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 10) {
+        $('.nav').addClass('affix');
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
